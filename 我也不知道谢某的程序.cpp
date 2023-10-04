adfb8a94740db90527dcd808838dfd18eaaff69f
John-Mathison-Neumann-Turing <146811815+John-Mathison-Neumann-Turing@users.noreply.github.com>
2023-10-04 15:35:42 +0800

    Add files via upload

---
+++ 我也不知道谢某的程序.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+	double a,b;
+	scanf("%lf %lf",&a,&b);
+	printf("%.5f",a/b);
+	return 0;
+}
---
+++ 随时取模 函数 模板.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long quick(long long a, long long b) {
+	long long res = 1;
+}
+
+while (b) {
+	if (b & 1)
+		res = (res * a) % M{
+		a = a(a * a) % M;
+		b >>= 1;
+	}
+	return res;
+
+
+	int main() {
+
+	}
+
+}
+
+void init() {
+	long long a / b mod M;
+	a *quick(b, M - 2) % M;
+}
+
